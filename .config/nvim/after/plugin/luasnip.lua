4058fea9a5cceedbfa2859d6f748d7d2ab67f8d6
dav-cho <chodavid@gmail.com>
2022-03-31 11:23:56 -0400

    extend js and ts snippets for js-react and ts-react

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -5,8 +5,12 @@ local function prequire(module)
   vim.notify(string.format("~ %s Call Error!", module))
 end
 
--- local ls = prequire('luasnip')
+local ls = prequire('luasnip')
 local from_vscode = prequire('luasnip.loaders.from_vscode')
+
+ls.filetype_extend('javascriptreact', {'javascript'})
+ls.filetype_extend('typescriptreact', {'typescript'})
 
 -- from_vscode.load({ paths = { '~/Library/Application Support/Code/User/snippets' }})
 from_vscode.lazy_load({ paths = { '~/Library/Application Support/Code/User/snippets' }})
+
