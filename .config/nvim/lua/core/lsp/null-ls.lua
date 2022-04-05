ea7a496d8d6e41e478a4a35196a85067ebd82cd4
dav-cho <chodavid@gmail.com>
2022-04-05 13:03:39 -0400

    global prettier install and goimports setup

--- .config/nvim/lua/core/lsp/null-ls.lua
+++ .config/nvim/lua/core/lsp/null-ls.lua
@@ -7,32 +7,28 @@ end
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
--- local completion = null_ls.builtins.completion
-
--- local formatting = prequire 'null-ls'.builtins.formatting
--- local diagnostics = prequire 'null-ls'.builtins.diagnostics
--- local completion = prequire 'null-ls'.builtins.completion
 
 null_ls.setup {
-    sources = {
-        -- formatting.eslint,
-        formatting.prettier.with {
-          extra_filetypes = { 'toml' },
-          -- extra_args = { '--single-quote', '--arrow-parens=avoid' },
-        },
-        formatting.black,
-        formatting.gofmt,
-        formatting.goimports,
-        formatting.stylua,
-        -- formatting.lua_format,
-
-        -- diagnostics.eslint,
-        diagnostics.zsh,
-
-        --completion.spell,
-
-        code_actions.gitsigns,
-        code_actions.eslint,
+  sources = {
+    formatting.prettier.with {
+      prefer_local = true,
+      command = '/Users/dav/.config/prettier/node_modules/.bin/prettier',
+      extra_filetypes = { 'toml' },
+      extra_args = { '--single-quote' },
+      -- extra_args = { '--single-quote', '--arrow-parens=avoid' },
     },
+    formatting.black,
+    -- formatting.gofmt,
+    formatting.goimports.with {
+      prefer_local = true,
+      command = '/Users/dav/go/bin/goimports'
+    },
+    -- formatting.stylua,
+
+    diagnostics.zsh,
+
+    code_actions.gitsigns,
+    code_actions.eslint,
+  },
 }
 
