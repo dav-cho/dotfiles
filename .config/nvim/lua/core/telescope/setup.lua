07c1454df242461290d1e12a48a919efdbb8aeed
dav-cho <chodavid@gmail.com>
2022-07-01 08:35:09 -0400

    tweak layout settings

--- .config/nvim/lua/core/telescope/setup.lua
+++ .config/nvim/lua/core/telescope/setup.lua
@@ -11,17 +11,43 @@ local actions = prequire 'telescope.actions'
 telescope.setup {
   defaults = {
     sorting_strategy = 'ascending',
+
     layout_strategy = 'flex',
     layout_config = {
       prompt_position = 'top',
+
+      horizontal = {
+        preview_width = function(_, cols, _)
+          if cols > 180 then -- (> 226 columns iterm JetBrainsMono Nerd Font size 14)
+            return math.floor(cols * 0.65)
+          elseif cols > 150 then -- (> 188 iterm JetBrainsMono Nerd Font size 14)
+            return math.floor(cols * 0.55)
+          else
+            return math.floor(cols * 0.5)
+          end
+        end,
+      },
+
+      vertical = {
+        mirror = true,
+        preview_height = 0.5,
+        preview_cutoff = 40, -- > (44 iterm JetBrainsMono Nerd Font size 14)
+      },
+
+      flex = {
+        flip_columns = 150,
+      },
     },
+
     color_devicons = true,
+
     mappings = {
       i = {
         ['<C-j>'] = actions.move_selection_next,
         ['<C-k>'] = actions.move_selection_previous,
       }
     },
+
     file_ignore_patterns = { '^.git/', '^node_modules/' },
   },
 
