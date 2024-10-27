6be9861a0ce3f540d21d970d7d9ea1906a110b21
dcho <dcho.null@gmail.com>
2024-10-27 09:47:44 -0400

    yabai: swap warp/stack

--- yabai/float-center.sh
+++ yabai/float-center.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-window=$(yabai -m query --windows --window)
-display=$(yabai -m query --displays --window)
+window=$(yabai -m query --windows --window | jq -r '.frame')
+display=$(yabai -m query --displays --window | jq -r '.frame')
 coords="$(
-	jq \
-		--argjson window "${window}" \
-		--argjson display "${display}" \
-		-nr '(($display.frame | .x + .w / 2) - ($window.frame.w / 2) | tostring)
-		    + ":"
-		    + (($display.frame | .y + .h / 2) - ($window.frame.h / 2) | tostring)'
+    jq \
+        --argjson window "${window}" \
+        --argjson display "${display}" \
+        -nr '($display.x + ($display.w - $window.w) / 2 | tostring)
+            + ":"
+            + ($display.y + ($display.h - $window.h) / 2 | tostring)'
 )"
 
 yabai -m window --move "abs:${coords}"
