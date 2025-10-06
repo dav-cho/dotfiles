44ebcf1f9331700fbe93f5e5e820301788337d61
dcho <dcho.null@gmail.com>
2025-10-06 15:08:16 -0400

    nvim: update ftplugin keymaps
    - add print line for js/ts
    - remove buffer opt for python maps

--- nvim/after/ftplugin/typescriptreact.lua
+++ nvim/after/ftplugin/typescriptreact.lua
@@ -1,3 +1,6 @@
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
+
+vim.keymap.set("n", "<Leader>po", "oconsole.log('-'.repeat(100));<Esc>", { desc = "print line below" })
+vim.keymap.set("n", "<Leader>pO", "Oconsole.log('-'.repeat(100));<Esc>", { desc = "print line above" })
